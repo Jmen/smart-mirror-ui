3206ddccbc481226f61740f982b1719e806c1f14
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2020-05-27 23:33:10 +0100

    create react app starter

---
+++ build/precache-manifest.c4e639a7bee2190e9bb2402a62d66853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f93956ae66d4a4a63a2b360d4b21518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6acabb6b2f21154b8a76",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6213dabc8ea60fbd88d9",
+    "url": "/static/js/2.470eab55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.470eab55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6acabb6b2f21154b8a76",
+    "url": "/static/js/main.6bf4adf3.chunk.js"
+  },
+  {
+    "revision": "69b235845e9810179196",
+    "url": "/static/js/runtime-main.c7c703ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
